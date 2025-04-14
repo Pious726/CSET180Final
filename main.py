ec4d3d02f0caaed6764baa269ab0ecf9b76cbb1b
Tyler M. <tjmcdowell726@gmail.com>
2025-04-14 07:55:14 -0400

    made template code

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, url_for, redirect
+from sqlalchemy import create_engine, text
+import random
+import bcrypt
+ 
+app = Flask(__name__)
+conn_str = "mysql://root:cset155@localhost/shop"
+engine = create_engine(conn_str, echo=True)
+conn = engine.connect()
+
+@app.route('/')
+def loadapp():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
